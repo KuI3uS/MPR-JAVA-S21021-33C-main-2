31ccc671ae9862c647bee717a64733eea209a955
KuI3uS <kuba.marcinkowski98@wp.pl>
2023-01-21 15:29:20 +0100

    git commit 1

---
+++ src/main/java/com/example/MPRJAVAS2102133C/MprJavaS2102133CApplication.java
@@ -0,0 +1,13 @@
+package com.example.MPRJAVAS2102133C;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MprJavaS2102133CApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MprJavaS2102133CApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MPRJAVAS2102133C/MprJavaS2102133CApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MPRJAVAS2102133C;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MprJavaS2102133CApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
